8663081478b9b7f33bd6058df968b777c13205cf
Nmohd <najeebu50@gmail.com>
2022-04-18 23:49:28 +0530

    ok done

---
+++ script.js
@@ -0,0 +1,17 @@
+(function () {
+  "use strict";
+  document
+    .getElementById("convert")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const distance = parseFloat(document.getElementById("distance").value);
+      const answer = document.getElementById("answer");
+      if (distance) {
+        //alert(distance);
+        const conversion = (distance * 1.609344).toFixed(3);
+        answer.innerHTML = `<h2>The ${distance} miles converts to ${conversion} kilometers </h2>`;
+      } else {
+        answer.innerHTML = `<h2>Please provide a number!</h2>`;
+      }
+    });
+})();
